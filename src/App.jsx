57e73edc40ec992470e08d41760ee3d3b661c7bf
Akshith <veerisettyakshith@gmail.com>
2025-07-21 22:46:36 +0530

    Added speed insight to start collecting performance metrics

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RecipeDetail from "./pages/RecipeDetail";
 import AboutPage from "./pages/AboutPage";
 import DotGrid from "./background/DotGrid";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
