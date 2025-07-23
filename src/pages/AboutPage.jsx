e6dcbaffaf58ee49028bbd67a9831d2bd416bd89
Akshith <veerisettyakshith@gmail.com>
2025-07-23 17:37:52 +0530

    changed some icons

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -34,17 +34,14 @@ function AboutPage() {
 
         <div className="journey-card">
           <div className="card-icon">
-            <svg
+            <img
               className="cooking-icon"
-              viewBox="0 0 24 24"
-              width="48"
-              height="48"
-            >
-              <path
-                fill="currentColor"
-                d="M8.1 13.34l2.83-2.83L3.91 3.5c-1.56 1.56-1.56 4.09 0 5.66l4.19 4.18zm6.78-1.81c1.53.71 3.68.21 5.27-1.38 1.91-1.91 2.28-4.65.81-6.12-1.46-1.46-4.2-1.1-6.12.81-1.59 1.59-2.09 3.74-1.38 5.27L3.7 19.87l1.41 1.41L12 14.41l6.88 6.88 1.41-1.41L13.41 13l1.47-1.47z"
-              />
-            </svg>
+              src="/src/assets/spoon_and_knife.svg"
+              alt="Spoon and Knife"
+              width={50}
+              height={50}
+              style={{ display: "block" }}
+            />
           </div>
           <h2>Crafted with Passion</h2>
           <p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,12 +59,13 @@ function HomePage() {
       <section className="welcome-section">
         <div className="welcome-content">
           <div className="welcome-icon">
-            <svg viewBox="0 0 24 24" width="48" height="48">
-              <path
-                fill="currentColor"
-                d="M8.1 13.34l2.83-2.83L3.91 3.5c-1.56 1.56-1.56 4.09 0 5.66l4.19 4.18zm6.78-1.81c1.53.71 3.68.21 5.27-1.38 1.91-1.91 2.28-4.65.81-6.12-1.46-1.46-4.2-1.1-6.12.81-1.59 1.59-2.09 3.74-1.38 5.27L3.7 19.87l1.41 1.41L12 14.41l6.88 6.88 1.41-1.41L13.41 13l1.47-1.47z"
-              />
-            </svg>
+            <img
+              src="/src/assets/spoon_and_knife.svg"
+              alt="Spoon and Knife"
+              width={80}
+              height={80}
+              style={{ display: "block" }}
+            />
           </div>
           <h1>Create Your Own Recipe</h1>
           <p>
